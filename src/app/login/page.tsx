2da536d4f84545dc64a3dc2e05fbacd284c2a705
kamlesh <alex20150001969@gmail.com>
2025-02-21 21:11:06 +0530

    console.log

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,7 +21,6 @@ const AdminLogin: React.FC = () => {
     try {
       const token =  await getCaptchaToken()
       const res = await adminLogin(passkey,mail,token);
-      console.log(res,"res");
       if(!res.success){
         setError(res.message);
       }else{
