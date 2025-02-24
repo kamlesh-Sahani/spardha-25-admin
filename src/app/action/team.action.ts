7fba7910ab2551f64b76e5eeda94e111188f985d
kamleh_sahani <126887367+kamlesh-Sahani@users.noreply.github.com>
2025-02-24 22:36:46 +0530

    Update team.action.ts

--- src/app/action/team.action.ts
+++ src/app/action/team.action.ts
@@ -419,7 +419,7 @@ export const deleteTeam = async (_id: string) => {
       };
     }
     team.isDeleted = true;
-    await team.save();
+  await team.save({validateBeforeSave:false});
     return {
       message: `successfuly Team ${team.teamID} Deleted`,
       success: true,
